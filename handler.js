df42a2b105c51fec87ff0bd8ca669e1b458ece43
Mabel Peña <mabel@sardius.media>
2023-09-05 14:26:25 -0400

    hello function added

---
+++ handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  var message = 'Hello World';
+  const name = event.queryStringParameters && event.queryStringParameters.name;
+  if (name !== null) {
+    message = 'Hello ' + name;
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: message,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
